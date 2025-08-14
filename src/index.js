f07f3355ed7e991eb7c49e2d41210d256689ffb9
carlotescabia <carlotaescabiariera@gmail.com>
2025-08-14 21:53:59 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import Apps from './Apps';
+import './index.css'; // opcional, solo si tienes CSS
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
-
+root.render(<Apps />);
